518944f696cd3f58cd0ec1220707a89d5fe21fe6
Wai Linn Aung <waia00198@gmail.com>
2023-12-27 15:30:13 +0630

    change tools name

--- src/utils/listArray.js
+++ src/utils/listArray.js
@@ -1,8 +1,8 @@
 const main = ["music", "sport", "business"];
 const partner = ["Our Partner", "Learn more", "Join"];
 const tools = [
-  "INDEX for Advertise",
+  "INDEX for Advertisement",
   "INDEX for Business",
-  "INDEX for Partner",
+  "INDEX for Partners",
 ];
 export { main, partner,tools };
